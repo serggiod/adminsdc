cbe4a48b74e44a57c53973612fb9cf42c1f9436e
Orlando Sergio Dominguez <serggio@msn.com>
2016-10-31 21:38:11 -0300

    0.0.3: Instalando DJango, virtualenv y mod_wsgi.

--- public/models-olds/adminadd.py
+++ public/models-olds/adminadd.py
@@ -3,14 +3,14 @@ from mod_python import apache
 #import string
 #import str
 
-def handler(req):
-    req.content_type = 'text/plain'
-    req.write("Hello World!")
-    #return apache.OK
+#def handler(req):
+#    req.content_type = 'text/plain'
+#    req.write("Hello World!")
+#    #return apache.OK
 
-def adminadd(req):
-    req.content_type = 'text/plain'
-    req.write("Hello World!")
+#def adminadd(req):
+#    req.content_type = 'text/plain'
+#    req.write("Hello World!")
     #return apache.OK
 #def index(req):
 #    return sys.version
