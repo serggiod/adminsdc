a100b2cd12f4a96ec80260eeb2191d2745c1c145
Orlando Sergio Dominguez <serggio@msn.com>
2016-11-29 00:07:26 -0300

    0.0.10: Diagrama de casos de uso implementado.

--- public/controllers/actividadController.js
+++ public/controllers/actividadController.js
@@ -22,18 +22,14 @@ angular
 	};
 
 	$scope.modelo = {
-		id:'id',
-		tipo:'tipo',
-		titulo:'titulo',
-		actividad:'actividad',
-		requisitos:'requisitos',
-		estado:'estado',
-		fecha:'2016-10-02 11:30:15',
-		archivos:[
-			{id:1,archivo:'actividad1.jpg',tipo:'image/jpg'},
-			{id:2,archivo:'actividad1.jpg',tipo:'image/jpg'},
-			{id:3,archivo:'actividad1.jpg',tipo:'image/jpg'}
-		],
+		id:'',
+		tipo:'',
+		titulo:'',
+		actividad:'',
+		requisitos:'',
+		estado:'',
+		fecha:'',
+		archivos:[],
 		archivo:''
 	};
 
@@ -87,10 +83,38 @@ angular
 			display:false,
 			no:()=>{
 				$scope.dialogs.autorizarSubirArchivo.display=false;
+				delete $scope.dialogs.autorizarSubirArchivo.tmp;
+				$scope.dialogs.autorizarSubirArchivo.input.value='';
 			},
 			si:()=>{
+				for(i in $scope.dialogs.autorizarSubirArchivo.tmp){
+					$scope.modelo.archivos.push($scope.dialogs.autorizarSubirArchivo.tmp[i]);
+				}
+				$scope.dialogs.autorizarSubirArchivo.input.value='';
 				$scope.dialogs.autorizarSubirArchivo.display=false;
-			}
+				delete $scope.dialogs.autorizarSubirArchivo.tmp;
+			},
+			onchange:(input)=>{
+				$scope.dialogs.autorizarSubirArchivo.input = input;
+				$scope.dialogs.autorizarSubirArchivo.tmp = [];
+				for(i in input.files){
+					var file = input.files[i];
+					console.log(file);
+					reader = new FileReader();
+					reader.readAsDataURL(file);	
+					reader.onload = (img)=>{
+						archivo = {
+							id:i,
+							archivo:img.target.result,
+							tipo:file.type,
+							nombre:file.name,
+							size:file.size,
+							resource:'local'
+						};
+						$scope.dialogs.autorizarSubirArchivo.tmp.push(archivo);
+					};
+				}
+			},
 		},
 		autorizarEliminarArchivo:{
 			display:false,
@@ -151,7 +175,23 @@ angular
 		actividadListar:{
 			display:false,
 			nuevaActividad:()=>{
+				d = new Date();
 				$scope.displayFalse();
+				$scope.modelo.id='';
+				$scope.modelo.tipo='';
+				$scope.modelo.titulo='';
+				$scope.modelo.actividad='';
+				$scope.modelo.requisitos='';
+				$scope.modelo.estado='';
+				$scope.modelo.fecha='';
+				$scope.modelo.fecha += d.getFullYear().toString()+'-';
+				$scope.modelo.fecha += (d.getMonth() +1).toString()+'-';
+				$scope.modelo.fecha += d.getDate().toString()+' ';
+				$scope.modelo.fecha += d.getHours().toString()+':';
+				$scope.modelo.fecha += d.getMinutes().toString()+':';
+				$scope.modelo.fecha += d.getSeconds().toString();
+				$scope.modelo.archivos=[];
+				$scope.modelo.archivo='';
 				$scope.forms.actividadNuevo.display=true;
 			},
 			visualizarActividad:(id)=>{
